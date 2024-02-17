ed329724a046d38828e0fd150db27ef2e58beda6
Alexandra Colorado <alexandrapena19@gmail.com>
2024-02-17 17:37:15 -0500

    Update sql.js
    Documentation for sqlForPartialUpdate was added.

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,6 +2,16 @@ const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
+/**
+ * `sqlForPartialUpdate` function is designed to dynamically generate SQL for partial updates based on the data provided and a mapping of JavaScript to SQL column names.
+ *
+ * dataToUpdate - The object containing the data to be updated.
+ * jsToSql - A mapping of JavaScript object keys to corresponding SQL column names.
+ * BadRequestError Throws a bad request error if there is no data to update.
+ * Returns an object containing the SQL SET clause and parameter values for the update query.
+ */
+
+
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
